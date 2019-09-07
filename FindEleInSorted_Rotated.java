a8b67d276131b929987aa54daed87716725ed14a
JainManjari <54873596+JainManjari@users.noreply.github.com>
2019-09-07 09:26:18 +0530

    Update FindEleInSorted_Rotated.java

--- FindEleInSorted_Rotated.java
+++ FindEleInSorted_Rotated.java
@@ -10,19 +10,69 @@ public class FindEleInSorted_Rotated {
 		{
 			a[i]=in.nextInt();
 		}
-		int count=0;
-		for(int i=0;i<n;i++)
-		{
-			if(i!=n-1)
-			{
-				if(a[i]<a[i+1])
-				{
-					count++;
-				}
-			}
-		}
-		System.out.print(count);
+		
 
+	}
+	
+	public static int pivot(int a[],int start,int last)
+	{
+		if(start>last)
+		{
+			return -1;
+		}
+		if(start==last)
+		{
+			return start;
+		}
+		int mid=(start+last)/2;
+		if(a[mid]<last && a[mid]>a[mid+1])
+		{
+			return mid;
+		}
+		if(a[mid]>start && a[mid]<a[mid-1])
+		{
+			return mid-1;
+		}
+		if(a[mid]>a[start]){
+			return pivot(a,mid+1,last);
+		}
+		return pivot(a,start,mid);
+	}
+	
+	public static int pivotBS(int a[], int start,int last,int key)
+	{
+		int p=pivot(a,0,a.length-1);
+		if(p==-1)
+		{
+			return binarySearch(a,0,a.length-1,key);
+		}
+		if(a[p]==key)
+		{
+			return key;
+		}
+		if(a[0]<=key)
+		{
+			return pivotBS(a,0,p,key);
+		}
+		return pivotBS(a,p+1,a.length-1,key);
+	}
+	
+	public static int binarySearch(int a[],int start,int last,int key)
+	{
+		if(start>last)
+		{
+			return -1;
+		}
+		int mid=(start+last)/2;
+		if(a[mid]==key)
+		{
+			return mid;
+		}
+		if(key<a[mid])
+		{
+			return binarySearch(a,start,mid,key);
+		}
+		return binarySearch(a,mid+1,last,key);
 	}
 
 }
