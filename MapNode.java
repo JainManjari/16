9a06fca0d8a748c5663c3b50341c10780affa157
JainManjari <54873596+JainManjari@users.noreply.github.com>
2019-09-12 08:19:04 +0530

    Update MapNode.java

--- MapNode.java
+++ MapNode.java
@@ -1,4 +1,15 @@
 
-public class MapNode {
+public class MapNode<K,V> {
+	
+	K key;
+	V value;
+	MapNode<K,V> next;
+	
+	public MapNode(K key, V value)
+	{
+		this.key=key;
+		this.value=value;
+		next=null;
+	}
 
 }
